0822b5f06d7eac09127957eb5bc45422e4973a41
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2018-12-12 16:45:14 +0100

    Re-work function to read all result json files + notebook with quick plot

---
+++ utils.py
@@ -0,0 +1,53 @@
+import glob
+import json
+import os
+
+import numpy as np
+import pandas as pd
+
+
+SCORE_TYPES = {
+    'regression': 'r2 score',
+    'binary-clf': 'average precision score',
+    'multiclass-clf': 'accuracy score'}
+
+
+def read_json(file):
+    with open(file, 'r') as f:
+        data = json.load(f)
+    return data
+
+
+def get_results_df(datadir):
+    """
+    Return a long format dataframe of all result
+    files included in the directory.
+
+    """
+    files = glob.glob(os.path.join(datadir, '*.json'))
+    df_all = []
+
+    for f in files:
+        f_dict = read_json(f)
+        df = pd.DataFrame(f_dict['results'])
+        # df = df.drop_duplicates(subset=df.columns[1:])
+        dataset = f_dict['dataset']
+        df['dataset'] = dataset
+        df['encoder'] = f_dict['encoder']
+        df['clf'] = f_dict['clf'][0]
+        df['n_splits'] = f_dict['n_splits']
+        df['results_file'] = os.path.split(f)[1]
+
+        dim_red_method = (('%s (d=%s) %s') % (
+            f_dict['encoder'],
+            f_dict['dimension_reduction'][1],
+            f_dict['dimension_reduction'][0]))
+        # min_d = min(f_dict['dimension_reduction'][1], min_d)
+        df['Dimension reduction method'] = dim_red_method
+
+        # clf type
+        df['score_type'] = SCORE_TYPES[f_dict['clf_type']]
+        df_all.append(df)
+
+    df_all = pd.concat(df_all, axis=0, ignore_index=True)
+    return df_all
