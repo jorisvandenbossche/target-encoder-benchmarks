47783dbe912cb1bcdcf9528d601d92ee2a9691bb
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2019-03-01 10:52:27 +0100

    add random dataset

--- datasets.py
+++ datasets.py
@@ -1,8 +1,10 @@
 import os
-import pandas as pd
 import datetime
 import warnings
 from constants import sample_seed
+
+import numpy as np
+import pandas as pd
 
 
 BENCHMARK_HOME = os.environ.get('BENCHMARK_HOME', '.')
@@ -13,6 +15,7 @@ def get_dataset(name):
         'employee_salaries': EmployeeSalariesDataset,
         'medical_charge': MedicalChargeDataset,
         'adult': AdultDataset,
+        'random-n=100-zipf=2': RandomDataset,
     }
     return DATASET_CLASSES[name]()
 
@@ -243,3 +246,121 @@ class EmployeeSalariesDataset(Dataset):
             d, '%m/%d/%Y').year for d
                                     in df['Date First Hired']]
         self.df = df
+
+
+class RandomDataset(Dataset):
+
+    name = 'random-n=100-zipf=2'
+
+    clf_type = 'binary-clf'
+
+    def __init__(self):
+        pass
+
+    def get_df(self):
+
+        X, y = make_classification_categorical(
+            n_samples=100000, n_categorical_features=3,
+            n_categories=[100, 10, 3], zipf_param=[2.0, 0.5, 0.1],
+            alpha=[.5, .8, 2.0], class_sep=0.5, random_state=42)
+
+        num_columns = ['num{}'.format(i) for i in range(20)]
+        cat_columns = ['cat1', 'cat2', 'cat3']
+        df = pd.DataFrame(X, columns=num_columns + cat_columns)
+        df[cat_columns] = df[cat_columns].astype(int)
+        df['target'] = y
+        self.df = df
+
+        self.col_action = {
+            'cat1': 'Special',
+            'cat2': 'OneHotEncoderDense',
+            'cat3': 'OneHotEncoderDense',
+            'target': 'y'}
+        self.col_action.update({key: 'Numerical' for key in num_columns})
+
+        return self
+
+
+###############################################################################
+# # Generate random categorical data
+###############################################################################
+
+
+def bounded_zipf(N, s):
+    import scipy.stats as stats
+
+    x = np.arange(1, N+1)
+    weights = x ** (-s)
+    weights /= weights.sum()
+    bounded_zipf = stats.rv_discrete(name='bounded_zipf', values=(x, weights))
+    return bounded_zipf
+
+
+def generate_categorical_given_y(y, n_categories=10, zipf_param=1.0, alpha=0.5,
+                                 random_state=None):
+    """
+    Approach:
+    - Construct frequency distribution for the categories
+    - Sample p-values (per category) for each target class
+    - For each target class:
+        - Combine the frequency distribution with p-values per category
+          to create frequency distribution for this target class.
+        - Sample from this set of categories (with replacement)
+          to fill X corresponding to this target class.
+    """
+    from scipy.stats import dirichlet
+    from sklearn.utils import check_random_state
+
+    generator = check_random_state(random_state)
+
+    n_samples = len(y)
+    classes = np.unique(y)
+    n_classes = len(classes)
+
+    # frequency distribution for the categories
+    dist = bounded_zipf(N=n_categories, s=zipf_param)
+    categories = np.arange(1, n_categories + 1)
+    categorical_distribution = dist.pmf(categories)
+
+    # generate p value (probability for each target class) for each category
+    dist_dirichlet = dirichlet([alpha] * n_classes)
+    ps = dist_dirichlet.rvs(n_categories, random_state=generator)
+
+    X = np.empty(n_samples, dtype='int')
+
+    for i in range(n_classes):
+        mask_i = y == classes[i]
+        counts_i = ps[:, i] * categorical_distribution * n_samples
+        vals_i = np.repeat(categories, counts_i.round().astype('int64'))
+        X[mask_i] = generator.choice(vals_i, mask_i.sum())
+
+    return X
+
+
+def make_classification_categorical(
+        n_samples, n_categorical_features,
+        n_categories=10, zipf_param=1.0, alpha=0.5, random_state=None,
+        **make_classification_kwargs):
+    from sklearn.datasets import make_classification
+    from sklearn.utils import check_random_state
+
+    generator = check_random_state(random_state)
+    X, y = make_classification(n_samples=n_samples, random_state=generator,
+                               **make_classification_kwargs)
+
+    feats = [X]
+
+    for i in range(n_categorical_features):
+        n_cats_ = (n_categories[i] if isinstance(n_categories, list) 
+                   else n_categories)
+        zipf_param_ = (zipf_param[i] if isinstance(zipf_param, list)
+                       else zipf_param)
+        alpha_ = alpha[i] if isinstance(alpha, list) else alpha
+
+        cat = generate_categorical_given_y(
+            y, n_categories=n_cats_, zipf_param=zipf_param_, alpha=alpha_)
+        feats.append(np.atleast_2d(cat).T)
+
+    X = np.concatenate(feats, axis=1)
+
+    return X, y
